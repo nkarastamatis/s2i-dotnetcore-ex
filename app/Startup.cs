bf71a43d78765778a5757e5d33b81c0588fcccbb
Karastamatis <NKarastamatis@roadnet.local>
2020-02-19 17:01:51 -0500

    disable https

--- app/Startup.cs
+++ app/Startup.cs
@@ -48,7 +48,7 @@ namespace app
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
